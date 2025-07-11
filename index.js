4ad3a1a886431e6d11ba334e169a77346196b3c9
jamesbraid11 <james_braid@hotmail.co.uk>
2025-07-11 10:59:48 +0100

    Player, HumanPlayer and ComputerPlayer classes created

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-class Player() { 
+// in order for Prompt to work on Js you have to install
+//  npm i prompt prompt-sync
+prompt = require("prompt-sync")();
 
-    constructor(name, result){
-        this.
-    }
+class Player {
+  constructor(name, result) {
+    this.name = prompt("PLease enter your name : ");
+  }
 
+  introduce() {
+    console.log(`Hi Welcome to this Dice Game ${this.name} `);
+  }
+}
+
+class HumanPlayer extends Player {
+  constructor(name, result) {
+    super(name, result);
+  }
+  rollDice() {
+    prompt("Enter 'go' to roll the dice: ").toLowerCase();
+    const result = Math.floor(Math.random() * 7);
+    this.result = result;
+    console.log(`${this.name} has rolled ${this.result}`);
+  }
+}
+
+class ComputerPlayer extends Player {
+  constructor(name = "Computer", choice) {
+    super(name, choice);
+  }
+  rollDice() {
+    const result = Math.floor(Math.random() * 7);
+    this.result = result;
+    console.log(`${this.name} has rolled ${this.result}`);
+  }
 }
